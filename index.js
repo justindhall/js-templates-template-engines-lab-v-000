01283beb10fff31e627085fe54a0afddda7cd017
Justin Hall <hall.justin.d@gmail.com>
2019-01-12 20:30:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 //templates
 
 function createPost(){
-  
+  //templates
+  var pageTemplate = _.template(document.getElementById("page-template").innerHTML);
+  var postTemplate
+  var commentsTemplate
 }
