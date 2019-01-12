2f65e5c2c8e333120f454dbdcd82b245ddd6fc59
Justin Hall <hall.justin.d@gmail.com>
2019-01-12 20:31:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,10 @@
 function createPost(){
   //templates
   var pageTemplate = _.template(document.getElementById("page-template").innerHTML);
+  
   var postTemplate = _.template(document.getElementById("post-template").innerHTML);
+  
   var commentsTemplate = _.template(document.getElementById("comments-template").innerHTML);
+  
+  
 }
